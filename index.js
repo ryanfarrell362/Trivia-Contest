3124b467d1f90a0971632a2864a99328ebffbd91
ryanfarrell362 <ryan.farrell362@gmail.com>
2021-05-28 23:15:04 -0400

    Initial bot set up

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const disbut = require('discord-buttons')(client);
+
+const config = require('./config.json');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(config.discord.token);
